55acb1de1f148db24eaf0135ae85e789fea04a03
jzlin11 <112991644+jzlin11@users.noreply.github.com>
2023-04-14 19:15:43 -0500

    Update dumpingEverything.py

--- dumpingEverything.py
+++ dumpingEverything.py
@@ -1,3 +1,43 @@
+from enum import Enum, auto
+
+cube = {}
+action_cost = -1
+
+
+class CellType(Enum):
+    normalCell = auto()
+    riskyCell = auto()
+    pickupCell = auto()
+    dropoffCell = auto()
+
+
+class Cell:
+    cellType = CellType.normalCell
+    position = (0, 0, 0)
+    reward = 0
+    capacity = 0
+    utility = 0
+
+
+def addCell(position, ctype=CellType.normalCell, reward=0, capacity=0, utility=0):
+    if position not in cube:
+        cell = Cell()
+        cell.cellType = ctype
+        cell.position = position
+        cell.reward = reward
+        cell.capacity = capacity
+        cell.utility = utility
+        cube.update({position: cell})
+
+
+for i in [(2, 2, 1), (3, 3, 2)]:
+    addCell(i, CellType.pickupCell, 14, 10)
+
+for i in [(1, 1, 1), (1, 1, 3), (3, 1, 1), (3, 2, 3)]:
+    addCell(i, CellType.dropoffCell, 14, 5)
+
+for i in [(2, 2, 2), (3, 2, 1)]:
+    addCell(i, CellType.riskyCell, 2*action_cost)
 
 
 class Agent:
@@ -18,14 +58,13 @@ def checkMoves(position):
     return moves
 
 
-def pickup(carry, reward):
+def pickup(carry, reward, position):
     if not carry:
-        return True, reward + 14
-    return carry, reward - 1
+        return True, reward + cube[position].reward
+    return carry, reward + action_cost
 
 
-def dropOff(carry, reward):
+def dropoff(carry, reward, position):
     if carry:
-        return False, reward + 14
-    return carry, reward - 1
-
+        return False, reward + cube[position].reward
+    return carry, reward + action_cost
