5218ea50a54d68b487b9409211e46bfe5d117913
jzlin11 <112991644+jzlin11@users.noreply.github.com>
2023-04-20 00:13:32 -0500

    Update train.py

--- train.py
+++ train.py
@@ -6,32 +6,34 @@ import sys
 
 
 # so we can run one agent first
-def updateAgent(agent):
+def updateAgent(agent, learn_func):
     old = agent.position
     agent.position = runPolicy(agent.algo, agent)  # will get next move base on policy
 
     action_result = perform_cell_action(agent.carrying_block, agent.position)  # x.q_table[:-1] as arg 2
     agent.carrying_block = action_result[0]
     reward = action_result[1]
+
     updateAgentRewards(agent, reward)
     updatePositionFrequency(agent)
 
-    updateQTable(agent, old, reward)  # change depending on how q_table works
+    updateQTable(agent, old, reward, learn_func)  # change depending on how q_table works
     # space for updating q_table
     # might shrink parameters to just agent, depending on how q_table is done
+
 
 
 iterations = 5000
 
 
-def train(cube):
+def train(cube, learn_func):
     # for loop for num of iterations?
     goal_reached = 0
     for i in range(iterations):
         for agent in agents:
             time.sleep(.05)
             rate(60)
-            updateAgent(agents[agent])
+            updateAgent(agents[agent], learn_func)
             cube.draw_agents(agents.values())
             if checkTerminalState():
                 goal_reached += 1
@@ -79,18 +81,18 @@ def addPathTaken(agent):
     agent.path = []
 
 
-def test():
+def test(learn_func):
     for agent in agents:
         addPathTaken(agents[agent])
     i = 0
     while not checkTerminalState() and i < iterations:
         for agent in agents:
-            updateAgent(agents[agent])
+            updateAgent(agents[agent], learn_func)
             agents[agent].path.append(agents[agent].position)
         i += 1
 
 
-def trainForPolicy(func):
+def trainForPolicy(func, learn_func):
     agents.clear()
     initializeCube()
     makeAgent('x', (1, 1, 1), func)
@@ -104,14 +106,14 @@ def trainForPolicy(func):
     visual.draw_cubes(pickups, dropoffs, risks)
 
     print('train results')
-    print('Goals: ', train(visual))
+    print('Goals: ', train(visual, learn_func))
     for agent in agents:
         print(agent)
         print('Rewards:', agents[agent].rewards)
         print()
         #print(agents[agent].frequencyTable)
     print()
-    test()
+    test(learn_func)
     print('Test Path')
     if agents['x'].path[0] == agents['x'].path[2] and agents['x'].path[1] == agents['x'].path[3]:
         print('looping')
@@ -122,8 +124,8 @@ def trainForPolicy(func):
     visual.invisible(agents.values())
 
 
-trainForPolicy(PRANDOM)
-trainForPolicy(PEXPLOIT)
-trainForPolicy(PGREEDY)
+trainForPolicy(PRANDOM, q_learn)
+trainForPolicy(PEXPLOIT, q_learn)
+trainForPolicy(PGREEDY, q_learn)
 
 done()
